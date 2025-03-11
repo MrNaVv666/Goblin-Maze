5f7b5b6d30f2a8f1743ab7f241e8111892e2baf5
Hubert Nowacki <H.Nowacki@protonmail.com>
2025-03-11 10:11:03 +0100

    Part 2

--- Assets/Scripts/Helper Scripts/MainMenuController.cs
+++ Assets/Scripts/Helper Scripts/MainMenuController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
@@ -14,5 +15,10 @@ public class MainMenuController : MonoBehaviour
     public void Back()
     {
         animator.Play("SlideOUT");
+    }
+
+    public void PlayLevel1()
+    {
+        SceneManager.LoadScene("Gameplay");
     }
 }
